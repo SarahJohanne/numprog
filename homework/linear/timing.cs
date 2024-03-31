ea708920a7268dee92f4f73530eaf6d041f0fe64
SarahJohanne <sarahglade.sjgc@gmail.com>
2024-03-31 15:37:05 +0200

    continued work on 1c

--- homework/linear/timing.cs
+++ homework/linear/timing.cs
@@ -1,11 +1,13 @@
-static class timing{
-	static int Main{
-
-		var random = new System.Random(4);
-		matrix A = new matrix(N);
-		for (int i=0; i<N; i++){
-			for (int j=0; j<N; j++)A[i,j] = random.NextDouble();}
-		QRGS.decomp(A);
-	return 0;
+using static System.Console;
+class timing{
+public static int Main(string[] args){
+        int N = args;
+	System.Random random = new System.Random(4);
+	matrix A = new matrix(N);
+	for (int i=0; i<N; i++){
+		for (int j=0; j<N; j++){A[i,j] = random.NextDouble();}
 	}
+	QRGS.decomp(A);
+return 0;
+}
 }
