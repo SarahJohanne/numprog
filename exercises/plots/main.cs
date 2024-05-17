d956f407f25c4b3c8c4fa3961c3225779f760617
SarahJohanne <sarahglade.sjgc@gmail.com>
2024-05-17 14:50:30 +0200

    added exercise plot

--- exercises/plots/main.cs
+++ exercises/plots/main.cs
@@ -1,8 +1,17 @@
 class main{
 public static int Main(){
-	for (double x=-3;x<=3;x+=0.125){
+	for (double x=0.001;x<=3;x+=0.125){
 	System.Console.WriteLine($"{x} {sfuns.erf(x)}");	
 	}
+	System.Console.WriteLine($"\n");
+	for (double x=0.125;x<=3;x+=0.126){
+	System.Console.WriteLine($"{x} {sfuns.gamma(x)}");	
+	}
+	System.Console.WriteLine($"\n");
+	for (double x=0.001;x<=3;x+=0.125){
+	System.Console.WriteLine($"{x} {sfuns.lngamma(x)}");	
+	}
+
 return 0;
 }
 
--- exercises/plots/sfuns.cs
+++ exercises/plots/sfuns.cs
@@ -13,7 +13,14 @@ return 1-sum*System.Math.Exp(-x*x);
 public static double gamma(double x){
 if(x<0)return PI/Sin(PI*x)/gamma(1-x);
 if(x<9)return gamma(x+1)/x;
-double lngamma=x*Log(x+1/(12*x-1/x/10))-x+Log(2*PI/x)/2;
+double lngamma = x*Log(x+1/(12*x-1/x/10))-x+Log(2*PI/x)/2;
 return Exp(lngamma);
 }
+
+public static double lngamma(double x){
+if(x<=0) return double.NaN;
+if(x<9) return lngamma(x+1)-Log(x);
+return x*Log(x+1/(12*x-1/x/10))-x+Log(2*PI/x)/2;
+}
+
 }//sfuns
